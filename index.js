16ce2f090c2b0f90e4aac43271b19f26405ac6e0
pokeruler <kevinmujica26@gmail.com>
2018-04-17 22:07:25 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require ('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+    
+    if(message.content == 'fag') {
+        message.reply('red, https://cdn.discordapp.com/attachments/386275238050004992/435633931078074389/15202234103141925776752_big.png');
+    }
+
+});
+
+bot.on('message', (message) => {
+    
+    if(message.content == 'monkey') {
+        message.reply('https://imgur.com/DpvWdve');
+    }
+
+});
+
+bot.on('message', (message) => {
+    
+    if(message.content == 'indian') {
+        message.reply('https://imgur.com/a/CZvtd');
+    }
+
+});
+
+bot.on('message', (message) => {
+    
+    if(message.content == 'whore') {
+        message.reply('https://cdn.discordapp.com/attachments/232182900311851010/298234141604970496/654034_179.png');
+    }
+
+});
+
+bot.on('message', (message) => {
+    
+    if(message.content == 'poke') {
+        message.reply('https://cdn.discordapp.com/attachments/426494967569973250/435632962948825089/IMG_1904.JPG');
+    }
+
+});
+
+bot.on('message', (message) => {
+    
+    if(message.content == 'help meme') {
+        message.reply('Here fag, these are the commands: fag, poke, whore, indian, and monkey');
+    }
+
+});
+
+bot.login('NDM1NjMyMzMxODQ0MzU0MDU5.DbbxvQ.LuOxBsO-XaxZvtecDBopjk7wp4Q');
